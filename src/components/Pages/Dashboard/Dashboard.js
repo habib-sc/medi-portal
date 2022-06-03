62a6fad2405838b678879c428178b68876c0b5be
habib-sc <habib97.cs@gmail.com>
2022-06-03 10:48:22 +0600

    active menu fix

--- src/components/Pages/Dashboard/Dashboard.js
+++ src/components/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import './Dashboard.css';
 
 const Dashboard = () => {
     const { pathname } = useLocation();
+
+    let activeClassName = "active-sidebar-item";
+
+    let activeStyle = {
+        backgroundColor: "#FDAD64",
+        padding: '5px',
+        color: 'white',
+        borderRadius: '6px',
+        width: '100%'
+      };
+
     return (
         <div className='dashboard-container'>
             <div className='dashboard-left'>
@@ -11,43 +22,55 @@ const Dashboard = () => {
                     {/* Admin route item  */}
                     { pathname.includes('/dashboard/admin') &&
                     <>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/doctors' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                            <Link to='/dashboard/admin/doctors'>Doctors</Link>
+                           <span> Doctors</span>
+                        </NavLink>
                         </li>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/staffs' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                            <Link to='/dashboard/admin/staffs'>Staffs</Link>
+                            <span>Staffs</span>
+                            </NavLink>
                         </li>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/admins' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                            <Link to='/dashboard/admin/admins'>Admins</Link>
+                            <span>Admins</span>
+                            </NavLink>
                         </li>
 
 
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/add-doctor' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                            <Link to='/dashboard/admin/add-doctor'>Add Doctor</Link>
+                            <span>Add Doctor</span>
+                            </NavLink>
                         </li>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/add-staff' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                            <Link to='/dashboard/admin/add-staff'>Add Staff</Link>
+                           <span>Add Staff</span>
+                           </NavLink>
                         </li>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/admin/add-admin' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                            <Link to='/dashboard/admin/add-admin'>Add Admin</Link>
+                            <span>Add Admin</span>
+                            </NavLink>
                         </li>
 
 
@@ -56,28 +79,34 @@ const Dashboard = () => {
                     {/* Doctor route item  */}
                     { pathname.includes('/dashboard/doctor') &&
                     <>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/doctor/patients' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                            <Link to='/dashboard/doctor/patients'>patients</Link>
+                            <span>patients</span>
+                        </NavLink>
                         </li>
                     </>}
 
                     {/* Staff route item  */}
                     { pathname.includes('/dashboard/staff') &&
                     <>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/staff/patients' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                            <Link to='/dashboard/staff/patients'>patients</Link>
+                           <span>patients</span>
+                        </NavLink>
                         </li>
-                        <li className='list-item'>
+                        <li>
+                        <NavLink to='/dashboard/staff/add-patient' className='list-item' style={({ isActive }) => isActive ? activeStyle : undefined}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="edit-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                            <Link to='/dashboard/staff/add-patient'>Add patient</Link>
+                            <span>Add patient</span>
+                        </NavLink>
                         </li>
                     </>}
 
